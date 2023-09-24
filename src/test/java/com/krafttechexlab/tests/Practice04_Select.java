6dfef830c50caa875ba30e9bbe42892f754caeef
emrekcmn <emre.kcmn15@gmail.com>
2023-09-24 16:38:09 +0300

    Select test2 is added

--- src/test/java/com/krafttechexlab/tests/Practice04_Select.java
+++ src/test/java/com/krafttechexlab/tests/Practice04_Select.java
@@ -3,6 +3,7 @@ package com.krafttechexlab.tests;
 import com.krafttechexlab.pages.SelectPage;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -23,5 +24,21 @@ public class Practice04_Select extends TestBase{
         for (WebElement option : options) {
             System.out.println("option.getText() = " + option.getText());
         }
+    }
+    @Test
+    public void test_selectByVisibleText(){
+        selectPage.navigateToModule("Forms","Select");
+        
+        Select select = new Select(selectPage.dropDownMenu);
+
+        //select two web element by visible text
+        select.selectByVisibleText("Two");
+
+        WebElement firstSelectedOption = select.getFirstSelectedOption();
+
+        String expected = "Two";
+        String actual =firstSelectedOption.getText();
+
+        Assert.assertEquals(actual,expected);
     }
 }
