8864c31e58376eb9d390683a4e7312e5341f2360
emrekcmn <emre.kcmn15@gmail.com>
2023-09-25 14:50:40 +0300

    FileUpload test is completed

--- src/test/java/com/krafttechexlab/pages/FileUploadPage.java
+++ src/test/java/com/krafttechexlab/pages/FileUploadPage.java
@@ -1,4 +1,15 @@
 package com.krafttechexlab.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class FileUploadPage extends BasePage{
+    @FindBy(id = "formFile")
+    public WebElement chooseFile;
+
+    @FindBy(css = "[type='submit']")
+    public WebElement submitFormButton;
+
+    @FindBy(css = "[role='alert']")
+    public WebElement fileUploadMessage;
 }
--- src/test/java/com/krafttechexlab/tests/Practice05_FileUpload.java
+++ src/test/java/com/krafttechexlab/tests/Practice05_FileUpload.java
@@ -1,4 +1,26 @@
 package com.krafttechexlab.tests;
 
+import com.krafttechexlab.pages.FileUploadPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Practice05_FileUpload extends TestBase{
+    FileUploadPage fileUploadPage = new FileUploadPage();
+    @Test
+    public void test1_fileUploadTest(){
+        fileUploadPage.navigateToModule("Forms","File Upload");
+        String projectPath= System.getProperty("user.dir");
+
+        String filePath= projectPath+"/src/test/resources/Xpath_CSS.txt";
+        fileUploadPage.chooseFile.sendKeys(filePath);
+        fileUploadPage.submitFormButton.click();
+
+        String fileName= "Xpath_CSS.txt";
+
+        String expected = "Your file named \" "+fileName+ " \" has been sent.";
+        String actual = fileUploadPage.fileUploadMessage.getText();
+
+        Assert.assertEquals(actual,expected);
+
+    }
 }
