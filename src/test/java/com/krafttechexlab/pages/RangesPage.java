2471b923d85b9e125bec03a191bfcfb9b0377f80
emrekcmn <emre.kcmn15@gmail.com>
2023-09-26 11:50:05 +0300

    Actions and JavascriptExecutor were used for ranges

--- src/test/java/com/krafttechexlab/pages/RangesPage.java
+++ src/test/java/com/krafttechexlab/pages/RangesPage.java
@@ -1,4 +1,12 @@
 package com.krafttechexlab.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class RangesPage extends BasePage{
+    @FindBy(id = "customRange1")
+    public WebElement range;
+
+    @FindBy(id = "disabledRange")
+    public WebElement disableRange;
 }
--- src/test/java/com/krafttechexlab/tests/Practice07_Ranges.java
+++ src/test/java/com/krafttechexlab/tests/Practice07_Ranges.java
@@ -1,4 +1,31 @@
 package com.krafttechexlab.tests;
 
+import com.krafttechexlab.pages.RangesPage;
+import com.krafttechexlab.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
 public class Practice07_Ranges extends TestBase{
+
+    @Test
+    public void test1_ranges() throws InterruptedException {
+        RangesPage rangesPage = new RangesPage();
+        rangesPage.navigateToModule("Forms","Ranges");
+
+        Actions actions = new Actions(Driver.get());
+
+        actions.dragAndDropBy(rangesPage.range, 200,0).perform();
+
+        Thread.sleep(500);
+        actions.dragAndDropBy(rangesPage.range, -700,0).perform();
+
+        Thread.sleep(500);
+        actions.clickAndHold(rangesPage.range).moveByOffset(400,0).perform();
+
+        Thread.sleep(500);
+        JavascriptExecutor js =(JavascriptExecutor) Driver.get();
+        js.executeScript("arguments[0].setAttribute('min','5')",rangesPage.disableRange);
+
+    }
 }
