8fc876388f52c1480eba3f71c564fc2f300c3092
emrekcmn <emre.kcmn15@gmail.com>
2023-09-27 12:43:24 +0300

    Fill out the form was continue

--- src/test/java/com/krafttechexlab/tests/Practice08_FillOutTheFullForm.java
+++ src/test/java/com/krafttechexlab/tests/Practice08_FillOutTheFullForm.java
@@ -10,6 +10,14 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Practice08_FillOutTheFullForm extends TestBase{
+
+    /** JavascriptExecutor methods used are:
+     *  js.executeScript("arguments[0].setAttribute('value','#edb879')",elementsPage.colorPicker); //pick color with JavascriptExecutor on the color picker
+     *  js.executeScript("arguments[0].scrollIntoView(true)",elementsPage.textareaInputBox);      //sroll with JavascriptExecutor
+     *  js.executeScript("arguments[0].setAttribute('checked','');",elementsPage.disabledRadioButton);
+     *  js.executeScript("arguments[0].checked = false",elementsPage.disableCheckedCheckBox3);
+     *  js.executeScript("arguments[0].checked = true",elementsPage.disableCheckBox4);
+     *  */
     @Test
     public void test1_fillOutTheFormOnElementsPage() throws InterruptedException {
         ElementsPage elementsPage= new ElementsPage();
@@ -36,11 +44,26 @@ public class Practice08_FillOutTheFullForm extends TestBase{
         Thread.sleep(1000);
 
         JavascriptExecutor js = (JavascriptExecutor) Driver.get();
-        js.executeScript("arguments[0].setAttribute('value','#edb879')",elementsPage.colorPicker);
 
-        Thread.sleep(1000);
+        js.executeScript("arguments[0].setAttribute('value','#edb879')",elementsPage.colorPicker);              //pick color with JavascriptExecutor on the color picker
+        Thread.sleep(500);
 
         elementsPage.textareaInputBox.sendKeys("Lorem Ipsum is simply dummy text of the printing and typesetting industry. ");
+
+        js.executeScript("arguments[0].scrollIntoView(true)",elementsPage.textareaInputBox);                     //sroll with JavascriptExecutor
+        Thread.sleep(500);
+
+        elementsPage.radioButton2.click();
+
+        js.executeScript("arguments[0].setAttribute('checked','');",elementsPage.disabledRadioButton);
+
+        elementsPage.checkBox1.click();
+        Thread.sleep(500);
+        elementsPage.checkBox2.click();
+
+        js.executeScript("arguments[0].checked = false",elementsPage.disableCheckedCheckBox3);
+        Thread.sleep(500);
+        js.executeScript("arguments[0].checked = true",elementsPage.disableCheckBox4);
 
     }
 }
