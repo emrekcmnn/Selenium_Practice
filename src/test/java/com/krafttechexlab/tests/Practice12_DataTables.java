e17954b3f8c60a9268b0b6434137b67bd1666514
emrekcmn <emre.kcmn15@gmail.com>
2023-10-04 16:56:36 +0300

    new tests are added in Data Tables

--- src/test/java/com/krafttechexlab/tests/Practice12_DataTables.java
+++ src/test/java/com/krafttechexlab/tests/Practice12_DataTables.java
@@ -38,5 +38,43 @@ public class Practice12_DataTables {
             System.out.println(header.getText());
         }
     }
+    @Test
+    public void test3_printTableSize(){
+        List<WebElement> allRowWithHeader = driver.findElements(By.xpath("//table[@id='table1']//tr"));
+        System.out.println("allRowWithHeader.size() = " + allRowWithHeader.size());
 
+        List<WebElement> allRowsWithoutHeader = driver.findElements(By.xpath("//table[@id='table1']//tbody/tr"));
+        System.out.println("allRowsWithoutHeader.size() = " + allRowsWithoutHeader.size());
+    }
+
+    private int getNumberOfColumns() {
+        List<WebElement> headers = driver.findElements(By.xpath("//table[@id='table1']//th"));
+        return headers.size();
+    }
+
+    private int getNumberOfRows() {
+        List<WebElement> allRowsWithoutHeader = driver.findElements(By.xpath("//table[@id='table1']//tbody/tr"));
+        return allRowsWithoutHeader.size();
+    }
+
+    @Test
+    public void test4_printAllCellsByIndex(){
+        //We want to write a code that it will print all cells one by one
+        int rowNumber = getNumberOfRows();
+        int columnNumber = getNumberOfColumns();
+
+        for (int i = 1; i <= rowNumber; i++) {
+            for (int j = 1; j <= columnNumber; j++) {
+                System.out.println(i + "/" + j + "-" + driver.findElement(By.xpath("//table[@id='table1']/tbody/tr[" + i + "]/td[" + j + "]")).getText());
+            }
+        }
+    }
+    private String getSpecificCellBasedOnNumbersThatIsGiven(int row, int column){
+        WebElement element = driver.findElement(By.xpath("//table[@id='table1']/tbody/tr[" + row + "]/td[" + column + "]"));
+    return element.getText();
+    }
+    @Test
+    public void test5(){
+        System.out.println(getSpecificCellBasedOnNumbersThatIsGiven(3, 5));
+    }
 }
